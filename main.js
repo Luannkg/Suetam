1136569be795172c80a39282ac331f7a0e1f2177
Luannkg <163136351+Luannkg@users.noreply.github.com>
2024-03-12 09:39:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://pbs.twimg.com/profile_images/1722278522457403392/Jm9ydMTA_400x400.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://fortnitebr.com.br/img/posts/82177343aa28a7f3ab5cc220f703e8b8.jpg"
+}
